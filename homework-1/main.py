31794b69e9022f809188b7b7cd66e1ed5c9daf65
Eugene Evstratev <levipupsik321@gmail.com>
2025-09-27 16:34:01 +0300

    скрипт отработал корректно, все данные внеслись в бд.

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,26 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+try:
+    with psycopg2.connect(host='localhost', database='north', user='postgres', password='12345') as conn:
+        with conn.cursor() as cur:
+            with open('north_data/customers_data.csv', 'r', encoding='utf-8') as file:
+                csv_dict = csv.DictReader(file)
+                for row in csv_dict:
+                    cur.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                                (row['customer_id'], row['company_name'], row['contact_name']))
+            with open('north_data/employees_data.csv', 'r', encoding='utf-8') as file:
+                csv_dict = csv.DictReader(file)
+                for row in csv_dict:
+                    cur.execute(
+                        "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s,%s, %s, %s)",
+                        (row['employee_id'], row['first_name'], row['last_name'], row['title'], row['birth_date'],
+                         row['notes']))
+            with open('north_data/orders_data.csv', 'r', encoding='utf-8') as file:
+                csv_dict = csv.DictReader(file)
+                for row in csv_dict:
+                    cur.execute(
+                        "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s,%s, %s)",
+                        (row['order_id'], row['customer_id'], row['employee_id'], row['order_date'], row['ship_city']))
+finally:
+    conn.close()
